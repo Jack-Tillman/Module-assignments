49e9cabea6f624b3e898b63cdc0195aa0b1aa07d
Jack-Tillman <johntillman345@gmail.com>
2023-08-31 14:30:07 -0400

    Completed functionality of block 24 and stylings!

--- Unit-3/Module-24/Module-24/src/App.jsx
+++ Unit-3/Module-24/Module-24/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import dog from './assets/Doge-Head-PNG-Clipart.png'
 import './App.css'
+import { useState } from 'react'
 import { puppyList } from './data.js'
 
 
 function App() {
   //useState returns an array with 2 elements: getter function (puppies), and setter function
+  // eslint-disable-next-line no-unused-vars
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
@@ -12,26 +14,38 @@ function App() {
   console.log(featuredPup);
   return (
     <>
+      <div>
+          <img src={dog} className="dog-logo" alt="Dog logo" />
+      </div>
       <div className="App">
        {puppies.map((puppy) => {
-        return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        return (      
+            <p className="puppy-name" onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+            </p>
+            )
        })
        }
       </div>
       {
       featPupId && (
-        <div>
+        <div className="feature-card">
           <h2>{featuredPup.name}</h2>
           <ul>
             <li>Age: {featuredPup.age}</li>
             <li>Email: {featuredPup.email}</li>
           </ul>
+          <button onClick={() => {
+            //replace FeatPupId with null to un-render feature card
+            setFeatPupId(null);
+          }}>Close</button>
         </div>
-
       )
       }
     </>
   )
 }
 
-export default App
+export default App;
+
+       
