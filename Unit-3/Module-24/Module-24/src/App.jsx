53450f47abbe8f01cab92d73dd2420511a4c6aba
Jack-Tillman <johntillman345@gmail.com>
2023-08-31 13:21:46 -0400

    Added functionality to App, just need to add CSS stylings to be complete with workshop

--- Unit-3/Module-24/Module-24/src/App.jsx
+++ Unit-3/Module-24/Module-24/src/App.jsx
@@ -1,14 +1,35 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
+import { puppyList } from './data.js'
+
 
 function App() {
-  // const [count, setCount] = useState(0)
+  //useState returns an array with 2 elements: getter function (puppies), and setter function
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
-      <div>
-       
+      <div className="App">
+       {puppies.map((puppy) => {
+        return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+       })
+       }
       </div>
+      {
+      featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+
+      )
+      }
     </>
   )
 }
